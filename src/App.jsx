e353947e580bff06788c1b210bbb558d253e75c9
Sess254 <wamaritemoses@gmail.com>
2026-03-21 00:41:43 +0300

    eduction section component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import cvImage from './assets/cv.png';
+import './App.css'
+
+
+
+function App() {
+  const handleButtonClick  = () => {
+    console.log('edit');
+  }
+  return (
+    ""
+  )
+}
+
+export default Person
---
+++ src/components/EductionInfo.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function EducationInfo() {
+
+    const [editmode, setEditMode] = useState(true);
+    const [formData, setFormData] = useState({
+        fullName: "",
+        email: "",
+        phoneNumber: "",
+    });
+
+    
+    
+
+}
+
+
+export default EducationInfo;
