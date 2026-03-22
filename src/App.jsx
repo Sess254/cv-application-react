75c83874d1f1efddce7e372c26b1a49943db58c5
Sess254 <wamaritemoses@gmail.com>
2026-03-22 13:23:31 +0300

    added the education info component withe ability to add and edit information

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <>
       <Header />
       <GeneralInfo />
+      <EducationInfo />
     </>
   )
 }
--- src/components/EductionInfo.jsx
+++ src/components/EductionInfo.jsx
@@ -5,15 +5,76 @@ function EducationInfo() {
     const [formData, setFormData] = useState({
         school: "",
         course: "",
-        date: "",
+        startDate: "",
+        endDate: "",
     });
 
     const handleChange = (e) => {
-        const {name, value} = e.value;
+        const {name, value} = e.target;
         setFormData(prevData => (
             {...prevData, [name]: value}
         ));
     }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setEditMode(false);
+    }
+
+    return (
+        <div>
+            {ediMode ? (
+                <form className="form" onSubmit={handleSubmit}>
+                    <label htmlFor="school">School Name:</label>
+                    <input
+                        name="school"
+                        id="school"
+                        value={formData.school}
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="freeCodeCamp"
+                    />
+                    <label htmlFor="course">Course:</label>
+                    <input 
+                        type="text"
+                        id="course"
+                        name="course"
+                        value={formData.course}
+                        placeholder="Software Engineer"
+                        onChange={handleChange}
+                        required
+                    />
+                    <label htmlFor="startDate">From</label>
+                    <input 
+                        type="date"
+                        id="startDate"
+                        name="startDate"
+                        onChange={handleChange}
+                        value={formData.startDate}
+                    />
+                    <label htmlFor="endDate">To</label>
+                    <input 
+                        type="date"
+                        id="endDate"
+                        name="endDate"
+                        onChange={handleChange}
+                        value={formData.endDate}
+                    />
+                    <button className="save" type="submit">Save</button>
+                    <hr />
+                </form>
+            ) : (
+                <div className="preview">
+                    <p><strong>School Name: </strong> {formData.school}</p>
+                    <p><strong>Course:</strong> {formData.course}</p>
+                    <p><strong>From:</strong> {formData.startDate}</p>
+                    <p><strong>End:</strong> {formData.endDate}</p>
+                    <button className="edit" onClick={() => setEditMode(true)}>Edit</button>
+                    <hr />
+                </div>
+            )}
+        </div>
+    )
 }
 
 
